c14f325a929fc68317ff2f3a81dfda74dab70d9a
Atilla Cantay Gül <atillacantay@hotmail.com>
2022-01-07 02:05:51 +0300

    storyblok init nuxtjs

---
+++ components/Article.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-editable="blok" class="prose my-24 mx-auto" :style="articleBlok">
+    <h1>{{ blok.name }}</h1>
+    <p>{{ blok.intro }}</p>
+    <rich-text-renderer :document="blok.body" />
+  </div>
+</template>
+ 
+<script>
+export default {
+  props: {
+    blok: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      articleBlok: {
+        backgroundColor: this.$props.blok.background_color.color,
+      },
+    };
+  },
+};
+</script>
